b315079fff56dfcad9c7b41dc72836155077a92f
daiwenbo <daiwenbo@cn.panasonic.com>
2024-11-13 10:16:51 +0800

    冲突测试

--- src/main/java/com/test/controller/DwbController.java
+++ src/main/java/com/test/controller/DwbController.java
@@ -41,7 +41,7 @@ public class DwbController {
 			lock.writeLock().lock();
 			TestUser user = null;
 			user.setId(id);
-			user.setUserName("测试冲突解决用户" + id);
+			user.setUserName("测试chongtu用户" + id);
 			lock.writeLock().unlock();
 			return user;
 		} catch (Exception e) {
